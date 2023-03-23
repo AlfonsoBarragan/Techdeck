eae0548448b233b35209979db2014535365d5a91
Alfonso Barragán Carmona <32295133+AlfonsoBarragan@users.noreply.github.com>
2023-03-23 14:04:27 +0000

    Added comment to check style checker

--- routes.py
+++ routes.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+"""
+    Python file to declare routes.
+
+    #TODO replace with a command declarations
+"""
+
 deck_folder = "Decks"
 graphics_folder = "graphics"
 resources_folder = "resources"
